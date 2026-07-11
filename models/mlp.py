0af752e60d9e7b419e09bdce16b8d1cda82648a2
Vivek Vijayan <vivekvjyn@proton.me>
2026-07-11 19:40:09 +0530

    revert to ae

---
+++ models/mlp.py
@@ -0,0 +1,19 @@
+from tensorflow.keras import layers, models
+
+
+class MLP(models.Model):
+    def __init__(self, dense_units, activations, input_dim, latent_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.dense_layers = [
+            layers.Dense(u, activation=a)
+            for u, a in zip(dense_units, activations)
+        ]
+        self.build(input_shape=(None, input_dim))
+
+    def call(self, x):
+        for layer in self.dense_layers:
+            x = layer(x)
+        return x
+
+    def get_config(self):
+        return super().get_config()
